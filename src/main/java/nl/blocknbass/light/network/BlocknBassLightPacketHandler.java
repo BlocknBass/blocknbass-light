1248afd22c1bc10c8bc3890a418650244f32826b
Geert Custers <geert.aj.custers@gmail.com>
2020-05-21 11:10:32 +0200

    Parse protobuf message and print pan

--- src/main/java/nl/blocknbass/light/network/BlocknBassLightPacketHandler.java
+++ src/main/java/nl/blocknbass/light/network/BlocknBassLightPacketHandler.java
@@ -1,16 +1,27 @@
 package nl.blocknbass.light.network;
 
+import com.google.protobuf.InvalidProtocolBufferException;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.LiteralText;
 import nl.blocknbass.core.BlocknBassPacketHandler;
 import nl.blocknbass.core.proto.MessageProto;
+import nl.blocknbass.light.proto.LightMessageProto;
 
 public class BlocknBassLightPacketHandler implements BlocknBassPacketHandler {
 
     @Override
     public void handle(MessageProto.Message message, MinecraftClient minecraftClient) {
+        LightMessageProto.LightMessage lightMessage;
+        try {
+            lightMessage = message.getMessage().unpack(
+                    LightMessageProto.LightMessage.class);
+        } catch (InvalidProtocolBufferException e) {
+            System.err.println("Couldn't decode light protobuf!");
+            return;
+        }
+
         minecraftClient.execute(() -> MinecraftClient.getInstance().player.addChatMessage(
-                new LiteralText("Hello from light mod"), false)
+                new LiteralText("Pan value: " + lightMessage.getPan()), false)
         );
     }
 }
